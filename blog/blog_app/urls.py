b4e50428dc038cac20177c176f7043a8d16d0a78
Aditya4700 <adityakolekar047@gmail.com>
2024-09-20 00:36:56 +0530

    initial commit

--- blog/blog_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('run-test/', views.run_test_view, name='run_test_view'),
-    path('create-comment/', views.create_comment_view, name='create_comment_view'),
-    path('create-user/', views.create_user_view, name='create_user_view'),
-
-]
